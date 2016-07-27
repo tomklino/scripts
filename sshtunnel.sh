febdc2f2d5b74f86f337024c0c8516744a6bd3bd
Tom Klinovsky <tomk@wix.com>
2016-07-27 14:30:07 +0300

    added conditionals to sshtunnel script

--- sshtunnel.sh
+++ sshtunnel.sh
@@ -2,11 +2,12 @@
 #creates a ssh tunnel and moves to background
 
 #if not enough attributes supplied, or supplied incorrectly, show usage
-#TODO: add an if statement to check the above
-echo "usage:\n"
-echo "sshtunnel.sh <connection> <local-port> <remote-port> [<goto-address>]\n"
-echo "<connection> is how to connect to the ssh. Example: 'root@192.168.0.1'\n"
-echo "<local-port> is the port on the machine, <remote-port> is the port on the ssh tunnel will go to\n"
-echo "<goto-address> is the address the ssh tunnel leads to. Default to 127.0.0.1 (stays on the remote machine)\n"
-
-ssh $1 -N -L $2:127.0.0.1:$3
+if [[ $(echo $1 | egrep "^[0-9a-zA-Z]+@[0-9a-zA-Z\\.]+$" | wc -l) = 0 ]] || [[ $(echo $2 | egrep "^[0-9]+$" | wc -l) = 0 ]] || [[ $(echo $3 | egrep "^[0-9]+$" | wc -l) = 0 ]] ; then
+	echo "usage:"
+	echo "sshtunnel.sh <connection> <local-port> <remote-port> [<goto-address>]"
+	echo "<connection> is how to connect to the ssh. Example: 'root@192.168.0.1'"
+	echo "<local-port> is the port on the machine, <remote-port> is the port on the ssh tunnel will go to"
+	echo "<goto-address> is the address the ssh tunnel leads to. Default to 127.0.0.1 (stays on the remote machine)"
+else
+	ssh $1 -N -L $2:127.0.0.1:$3
+fi
