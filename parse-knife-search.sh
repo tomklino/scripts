4961c221cfa84e3fe240a4ec3bdbae992a12cc9c
Tom Klinovsky <tom.klino@gmail.com>
2017-04-27 17:26:41 +0300

    script to parse the results of a knife search

---
+++ parse-knife-search.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+clearvars() { fqdn=""; ipaddress="";}
+
+#echo $1
+
+cat $1 | sed -r 's/^[ \t]+/---/' | while IFS= read -r line; do
+  if [[ $line ]] ; then
+    if [[ $line =~ ^--- ]]; then
+      ipaddress=$(sed -r 's/^---ipaddress:[^0-9]*([0-9\.]+).*$/\1/' <<< ${line});
+    else
+      fqdn=$(sed -r 's/^([^\:]+)\:/\1/' <<< $line)
+    fi
+  else
+    #line is empty, print what you've got and clear vars
+    printf "%s,%s\n" "$fqdn" "$ipaddress";
+    clearvars;
+  fi
+done
+
