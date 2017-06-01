a8e849327e12c906869d78e6431fa627a888216c
Tom Klinovsky <tom.klino@gmail.com>
2017-06-01 10:45:09 +0300

    fixed parse knife search script

--- parse-knife-search.sh
+++ parse-knife-search.sh
@@ -7,13 +7,13 @@ clearvars() { fqdn=""; ipaddress="";}
 cat $1 | sed -r 's/^[ \t]+/---/' | while IFS= read -r line; do
   if [[ $line ]] ; then
     if [[ $line =~ ^--- ]]; then
-      macaddress=$(sed -r 's/^---macaddress:[^0-9a-fA-F]*([0-9a-fA-F\:]+).*$/\1/' <<< ${line});
+      ipaddress=$(sed -r 's/^---macaddress:[^0-9a-fA-F]*([0-9a-fA-F\:]+).*$/\1/' <<< ${line});
     else
       fqdn=$(sed -r 's/^([^\:]+)\:/\1/' <<< $line)
     fi
   else
     #line is empty, print what you've got and clear vars
-    printf "%s,%s\n" "$fqdn" "$macaddress";
+    printf "%s,%s\n" "$fqdn" "$ipaddress";
     clearvars;
   fi
 done
