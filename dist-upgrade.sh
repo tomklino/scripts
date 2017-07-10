28d447729a565931c6d570b69185cd73f465f793
Tom Klinovsky <tom.klino@gmail.com>
2017-07-10 12:18:53 +0300

    a script for unattended dist upgrade to jessie

---
+++ dist-upgrade.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "upgrading dist in progress" > /tmp/chef.lock &&
+export APT_LISTCHANGES_FRONTEND=none &&
+export DEBIAN_FRONTEND=noninteractive &&
+echo 'libc6 libraries/restart-without-asking boolean true' | debconf-set-selections &&
+echo "executing wheezy to jessie" &&
+find /etc/apt -name "*.list" | xargs sed -i '/^deb/s/wheezy/jessie/g' &&
+echo "executing autoremove" &&
+apt-get -fuy --force-yes autoremove &&
+echo "executing clean" &&
+apt-get --force-yes clean &&
+echo "executing update" &&
+apt-get update &&
+echo "executing upgrade" &&
+apt-get --force-yes -o Dpkg::Options::="--force-confold" --force-yes -o Dpkg::Options::="--force-confdef" -fuy upgrade &&
+echo "executing dist-upgrade" &&
+apt-get --force-yes -o Dpkg::Options::="--force-confold" --force-yes -o Dpkg::Options::="--force-confdef" -fuy dist-upgrade
+rm /tmp/chef.lock
