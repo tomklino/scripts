a28a368cc2d2f80f2b144103ab007dd1e593cdfd
Tom Klinovsky <tom.klino@gmail.com>
2017-07-10 12:32:27 +0300

    it should not matter from where you run the script - it should always work

--- fix-liftoff-settings.sh
+++ fix-liftoff-settings.sh
@@ -1,4 +1,4 @@
 HEIGHT=$(xdpyinfo | grep dimensions | grep -Eo [0-9]+x[0-9]+ | head -n1 | sed -r 's/^[0-9]+x([0-9]+)$/\1/')
 WIDTH=$(xdpyinfo | grep dimensions | grep -Eo [0-9]+x[0-9]+ | head -n1 | sed -r 's/^([0-9]+)x[0-9]+$/\1/')
 
-sed "{/Fullscreen/s/1/0/;/Resolution\ Height/s/>0</>$HEIGHT</;/Resolution\ Width/s/>0</>$WIDTH</}" -i .config/unity3d/LuGus\ Studios/Liftoff/prefs
+sed "{/Fullscreen/s/1/0/;/Resolution\ Height/s/>0</>$HEIGHT</;/Resolution\ Width/s/>0</>$WIDTH</}" -i ${HOME}/.config/unity3d/LuGus\ Studios/Liftoff/prefs
