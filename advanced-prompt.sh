15915b9421edd6a2463312071a34d7e4fb120180
Tom Klinovsky <tom@kiwinet.co.il>
2018-09-17 10:18:50 +0300

    fixed kube prompt

--- advanced-prompt.sh
+++ advanced-prompt.sh
@@ -4,7 +4,7 @@ function color_my_prompt {
     local __git_branch_color="\[\033[31m\]"
     local __git_branch='`git branch 2> /dev/null | grep -e ^* | sed -E  s/^\\\\\*\ \(.+\)$/\(\\\\\1\)\ /`'
     local __kube_prompt_color="\[\033[36m\]"
-    local __kube_prompt='`kubectl config current-context 2>/dev/null | sed -E "s/^(.+)$/{\1}/"`'
+    local __kube_prompt='`kubectl config current-context 2>/dev/null | sed -E "s/^(.+)$/{\1} /"`'
     local __prompt_tail="\[\033[35m\]$"
     local __last_color="\[\033[00m\]"
     export PS1="$__user_and_host $__cur_location $__git_branch_color$__git_branch$__kube_prompt_color$__kube_prompt$__prompt_tail$__last_color "
