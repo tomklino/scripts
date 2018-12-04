ae5d168abaeb23050662ec4e0713c068010b5562
Tom Klinovsky <tom.klino@gmail.com>
2018-12-05 00:13:35 +0200

    fixed issue with bash

--- advanced-prompt.sh
+++ advanced-prompt.sh
@@ -7,6 +7,6 @@ function color_my_prompt {
     local __kube_prompt='`kubectl config current-context 2>/dev/null | sed -E "s/^(.+)$/{\1} /"`'
     local __prompt_tail="\[\033[35m\]$"
     local __last_color="\[\033[00m\]"
-    export PS1="\033]2;\w\007$__user_and_host $__cur_location $__git_branch_color$__git_branch$__kube_prompt_color$__kube_prompt$__prompt_tail$__last_color "
+    export PS1="$__user_and_host $__cur_location $__git_branch_color$__git_branch$__kube_prompt_color$__kube_prompt$__prompt_tail$__last_color "
 }
 color_my_prompt
