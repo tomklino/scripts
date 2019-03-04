89fb68e20fb5cb6a303eb491dc8ec250afb7301b
Tom Klinovsky <lutraman@gmail.com>
2019-03-04 14:23:33 +0200

    added jq aliases for kubernetes

--- jq-aliases-for-kubernetes.sh
+++ jq-aliases-for-kubernetes.sh
@@ -1,4 +1,7 @@
 alias jq-get-owner="jq '.items[] | { name: .metadata.name, owner: { kind: .metadata.ownerReferences[].kind, name: .metadata.ownerReferences[].name }}'"
 alias jq-get-env="jq '.spec.template.spec.containers[].env'"
-alias jq-get-volume-claim-templates="jq -C '.items[].spec.volumeClaimTemplates[]'"
+alias jq-get-volume-claim-templates="jq '.items[].spec.volumeClaimTemplates[]'"
+alias jq-get-labels="jq '.metadata.labels'"
+alias jq-get-selector="jq '.spec.selector'"
+alias jq-get-rules="jq '.spec.rules'"
 
