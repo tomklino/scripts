888bf93160ef54114d376bfdf4ea04eeb6a18140
Tom Klinovsky <lutraman@gmail.com>
2019-03-17 13:32:18 +0200

    improved wait-for-cmd

--- wait-for-cmd.sh
+++ wait-for-cmd.sh
@@ -1,16 +1,73 @@
 #!/bin/bash
+usage() {
+  exitcode="$1"
+  cat << USAGE >&2
+Usage:
+  $0 [-r retries] [-t timeout] -- command args
+  -t TIMEOUT | --timeout=timeout      Time to wait between retries
+  -t RETRIES | --retries=retries      Max amout of retries after which will exit with failure
+  -- COMMAND ARGS                     Command with args to test
+USAGE
+  exit "$exitcode"
+}
+
+while [ $# -gt 0 ]
+do
+  case "$1" in
+    -t)
+    TIMEOUT="$2"
+    if [ "$TIMEOUT" = "" ]; then break; fi
+    shift 2
+    ;;
+    --timeout=*)
+    TIMEOUT="${1#*=}"
+    shift 1
+    ;;
+    -r)
+    RETRIES="$2"
+    if [ "$RETRIES" = "" ]; then break; fi
+    shift 2
+    ;;
+    --retries=*)
+    RETRIES="${1#*=}"
+    shift 1
+    ;;
+    --)
+    shift
+    break
+    ;;
+    --help)
+    usage 0
+    ;;
+    *)
+    echo "Unknown argument: $1"
+    usage 1
+    ;;
+  esac
+done
+
 cmd="$@"
 
-echo "$cmd"
+if [[ -z "$TIMEOUT" ]]; then TIMEOUT=1; fi
+if [[ -z "$cmd" ]]; then
+  usage 1;
+fi
+
+echo "waiting for ${cmd}"
 function execute() {
   $($@)
   return $?
 }
 
+FAILS=0
 until execute "$cmd"; do
+  FAILS=$((FAILS+1))
+  if [[ ! -z ${RETRIES} ]] && [[ ${FAILS} == ${RETRIES} ]]; then
+    echo "maximum amout of retries (${RETRIES}) exceeded";
+    exit 1;
+  fi
   echo -n ".";
-  sleep 1;
+  sleep $TIMEOUT;
 done;
 
 echo '!'
-
