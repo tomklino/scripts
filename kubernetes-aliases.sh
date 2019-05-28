32c4ed62aec4ad4c299114c20a5e19e2551453bc
Tom Klinovsky <lutraman@gmail.com>
2019-05-28 11:34:46 +0300

    renamed kubernetes aliases file and added an alias

--- kubernetes-aliases.sh
+++ kubernetes-aliases.sh
@@ -1,7 +1,10 @@
+# jq common filters (when using kubectl <command> -o json)
 alias jq-get-owner="jq '{ name: .metadata.name, owner: { kind: .metadata.ownerReferences[].kind, name: .metadata.ownerReferences[].name }}'"
 alias jq-get-env="jq '.spec.template.spec.containers[].env'"
 alias jq-get-volume-claim-templates="jq '.items[].spec.volumeClaimTemplates[]'"
 alias jq-get-labels="jq '.metadata.labels'"
 alias jq-get-selector="jq '.spec.selector'"
 alias jq-get-rules="jq '.spec.rules'"
+
+alias k-server-version="kubectl version -o yaml | yq r - 'serverVersion.gitVersion'"
 
