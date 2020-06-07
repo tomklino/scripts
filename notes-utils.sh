85bb040f52f1924c304eca4d5114f3dcbdca2d00
Tom Klinovsky <tom.klino@gmail.com>
2020-06-07 11:42:44 +0300

    fixed a thing with summarize-week-notes function

--- notes-utils.sh
+++ notes-utils.sh
@@ -3,7 +3,7 @@
 function summarize-week-notes() {
   ( source ~/.workspace-notes.conf;
     cd ${WORKING_DIR};
-    find -type f -ctime -6 -not -size 0 \
+    find -type f -mtime -7 -not -size 0 \
     -printf "%T@ %Tc '%p'\n" | sort -n |\
      sed -nE "/txt'$/s/.*'(.*txt)'/\1/p" |\
     while read f; do echo "------${f}-------"; cat ${f}; done |\
