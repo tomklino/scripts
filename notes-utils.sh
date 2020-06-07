2eafba1d04c5393cc20f30243cd7071eae0fb901
Tom Klinovsky <tom.klino@gmail.com>
2020-06-07 11:31:40 +0300

    small script to help summarize the notes changed in the past week

---
+++ notes-utils.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+function summarize-week-notes() {
+  ( source ~/.workspace-notes.conf;
+    cd ${WORKING_DIR};
+    find -type f -ctime -6 -not -size 0 \
+    -printf "%T@ %Tc '%p'\n" | sort -n |\
+     sed -nE "/txt'$/s/.*'(.*txt)'/\1/p" |\
+    while read f; do echo "------${f}-------"; cat ${f}; done |\
+    less -X
+  )
+}
+
