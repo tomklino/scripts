10fda9d44e86532a9246c82306396f777fffd7f6
Tom Klinovsky <tom.klino@gmail.com>
2023-02-07 15:27:24 +0200

    reorganizing ws notes code
    adding auto wrap at 80 chars
    
    adding monthly note instead of daily general

--- workspaces-notes.sh
+++ workspaces-notes.sh
@@ -1,9 +1,8 @@
 CONF_FILE="${HOME}/.workspace-notes.conf"
-NUM_OF_WORKSPACES=4
-DATE=$(date +%F)
-MONTH=$(date +%B | awk '{print tolower($0)}')
+NUM_OF_WORKSPACES=4 # default. will be overwritten by the value in conf_file
 WINDOW_TITLE="Workspaces"
 
+### Loading conf ###
 if [[ ! -e "$CONF_FILE" ]]; then
   echo "no path defined for workspace notes working directory. type full path beginning with '/' or relative to home dir without '/':"
   read WORKING_DIR
@@ -15,23 +14,31 @@ fi
 
 source $CONF_FILE
 
+### Creating Monthly Dir ###
+MONTH=$(date +%B | awk '{print tolower($0)}')
 cd $WORKING_DIR || (echo "could not cd into $WORKING_DIR" && exit)
 if [[ ! -d ${MONTH}.d ]]; then
   mkdir ${MONTH}.d
 fi
 
 cd ${MONTH}.d || (echo "could not use ${MONTH}.d as a direcotry" && exit)
+MONTHLY_FILE="$(date +%B-%Y).txt"
+touch $MONTHLY_FILE
 
+### Creating Daily Files ###
+DATE=$(date +%F)
 if [[ ! -d workspaces-$DATE ]]; then
   mkdir workspaces-$DATE
 fi
 
 cd workspaces-$DATE
 
-for i in $(seq 1 $NUM_OF_WORKSPACES); do touch workspace-${i}.txt; done
-touch general.txt;
+NUM_OF_WORKSPACES=${1:-$NUM_OF_WORKSPACES}
+for i in $(seq 1 $NUM_OF_WORKSPACES); do
+    touch workspace-${i}.txt;
+done
 
 echo -e '\033]2;'$WINDOW_TITLE'\007'
 
-vim -o workspace-* general.txt
+vim "+tabdo windo set tw=80" -o workspace-* ../${MONTHLY_FILE}
 
