f953de0406558944bbc14c7588c4fa47b345316c
Tom Klinovsky <tom.klino@gmail.com>
2024-07-28 19:23:33 +0300

    kubectl wrapper recursion fix and no color when piping

--- kubectl-wrapper.sh
+++ kubectl-wrapper.sh
@@ -1,9 +1,13 @@
 # Experimental kubectl pager wrapper
-KUBECTL_CMD=$(which kubectl)
+KUBECTL_CMD=$(whereis -b kubectl | awk '{print $2}')
 
 function kubectl() {
   if awk '{for(i=1;i<=NF;i++) {if ($i == "-oyaml") exit 0}; exit 1}' <<< $@; then
-    $KUBECTL_CMD $@ | yq -C | less -KFX
+    if [ -t 1 ] ; then
+      $KUBECTL_CMD $@ | yq -C | less -KFX ;
+    else
+      $KUBECTL_CMD $@ | yq | less -KFX ;
+    fi
   else
     $KUBECTL_CMD $@ | less -KFX
   fi
