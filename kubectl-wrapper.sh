f03749ac5146c878133cb7cad4178d60fb3adb5f
Tom Klinovsky <tom.klino@gmail.com>
2024-07-29 15:24:59 +0300

    fix: editing in vim pipes to terminal

--- kubectl-wrapper.sh
+++ kubectl-wrapper.sh
@@ -2,6 +2,11 @@
 KUBECTL_CMD=$(whereis -b kubectl | awk '{print $2}')
 
 function kubectl() {
+  if awk '{for(i=1;i<=NF;i++) {if ($i == "edit") exit 0}; exit 1}' <<< $@; then
+    $KUBECTL_CMD $@
+    return $?
+  fi
+
   if awk '{for(i=1;i<=NF;i++) {if ($i == "-oyaml") exit 0}; exit 1}' <<< $@; then
     if [ -t 1 ] ; then
       $KUBECTL_CMD $@ | yq -C | less -KFX ;
