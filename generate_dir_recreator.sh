e7f9ecdfb644ccbe7c47ee30c254c73fb35af7cf
Tom Klinovsky <tom.klino@gmail.com>
2026-02-12 11:11:49 +0200

    generelized generate_dir_recreator

--- generate_dir_recreator.sh
+++ generate_dir_recreator.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+## Writes a bash script using heredocs to recreate a folder
+
 set -euo pipefail
 
 if [ $# -ne 1 ]; then
@@ -41,44 +43,3 @@ find "$SOURCE_DIR" -type f | while read -r file; do
     echo "EOF_$(echo "$rel_path" | tr '/' '_' | tr -d '.-')"
     echo ""
 done
-
-echo "echo \"\$TARGET_DIR\""
-echo ""
-echo "RANDOM_NUM=\$RANDOM"
-echo "CONTAINER_NAME=main_armis_observability_shipper_\$RANDOM_NUM"
-echo ""
-echo "docker run -d \\"
-echo "    --name \$CONTAINER_NAME \\"
-echo "    --memory 6g \\"
-echo "    --memory-swap 6g \\"
-echo "    --stop-timeout 60 \\"
-echo "    -p 12345-12349:12345 \\"
-echo "    --add-host host.docker.internal:host-gateway \\"
-echo "    -v /etc/armis/root_ca.crt:/etc/agent/root_ca.crt:ro \\"
-echo "    -v /proc:/host/proc:ro \\"
-echo "    -v /etc/armis/tenant.json:/etc/agent/tenant.json:ro \\"
-echo "    -v /sys:/host/sys:ro \\"
-echo "    -v /mnt/data/logs/:/logs:ro \\"
-echo "    -v /:/rootfs:ro \\"
-echo "    -v /var/run/docker.sock:/tmp/docker.sock:ro \\"
-echo "    -v /mnt/data/prometheus/observability_shipper/:/etc/agent/data:rw \\"
-echo "    -v /mnt/data/prometheus/sensors:/etc/agent/sensors:ro \\"
-echo "    -v \"\$TARGET_DIR\":/armis/deploy/services/observability_shipper/config:ro \\"
-echo "    -e AGENT_MODE=flow \\"
-echo "    -e LOG_LEVEL=info \\"
-echo "    -e DESCRIPTION=\"Observability Shipper responsible for shipping observability data to upstreams\" \\"
-echo "    -e OWNER=devops-infra \\"
-echo "    -e GROUP_OWNER=dev-infra \\"
-echo "    -e AGENT_DEPLOY_MODE=docker \\"
-echo "    -l com.armis.domain=infra \\"
-echo "    -l com.armis.image_type=observability_shipper \\"
-echo "    -l com.armis.metrics_scrape_port=12345 \\"
-echo "    -w /armis/deploy/services/observability_shipper/config \\"
-echo "    --entrypoint /armis/deploy/services/observability_shipper/run.sh \\"
-echo "    armis/observability_shipper:distribute"
-echo ""
-echo "echo \"Container \$CONTAINER_NAME started. Waiting 10 seconds and showing logs...\""
-echo "sleep 10"
-echo "docker logs \$CONTAINER_NAME"
-echo "docker kill \$CONTAINER_NAME"
-echo "docker rm \$CONTAINER_NAME"
